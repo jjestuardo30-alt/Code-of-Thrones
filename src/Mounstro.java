622f9f085d3f7011ae0f2e4eaf2fb447ecf25575
JulioAr <julioestuardo.arango@gracia.lasalle.cat>
2026-02-06 13:13:35 +0100

    Feat: Se implemeta 'Mounstro' implemetantdo su logica especifica  completa

--- src/Mounstro.java
+++ src/Mounstro.java
@@ -1,3 +1,22 @@
-public class Mounstro {
+
+public class Mounstro extends Entidad {
+    private int ferocidad;
+
+    public Mounstro(String nombre, int nivel, int puntosVida, int ferocidad) {
+        super(nombre, nivel, puntosVida);
+        this.ferocidad = ferocidad;
+    }
+
+    @Override
+    public void atacar() {
+
+        int daño = ferocidad + getNivel();
+        System.out.println(getNombre() + " ataca salvajemente! (Daño: " + daño + ")");
+    }
+
+    @Override
+    public int calcularDefensa() {
+        return ferocidad / 2;
+    }
 
 }
