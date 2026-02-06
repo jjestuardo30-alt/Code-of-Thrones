95c988ec22a6afc1ce060fe53d3162952353e5d4
JulioAr <julioestuardo.arango@gracia.lasalle.cat>
2026-02-06 09:00:07 +0100

    Feat: Se Implementa 'Guerrero' añadiendo su logica especifica completa

--- src/Guerrero.java
+++ src/Guerrero.java
@@ -1,3 +1,22 @@
-public class Guerrero {
+public class Guerrero extends Entidad {
+    private int resistencia;
+
+    public Guerrero(String nombre, int nivel, int puntosVida, int resistencia) {
+        super(nombre, nivel, puntosVida);
+        this.resistencia = resistencia;
+    }
+
+    @Override
+    public void atacar() {
+        int daño = getNivel() * 3;
+        System.out.println(getNombre() + "ataca con espada! (Daño: " + daño + ")");
+
+    }
+
+    @Override
+    public int calcularDefensa() {
+        return resistencia + (getNivel() / 2);
+
+    }
 
 }
