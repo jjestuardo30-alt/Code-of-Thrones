9605ca252c98ec9b4b465d646716b814ca93597c
JulioAr <julioestuardo.arango@gracia.lasalle.cat>
2026-02-06 13:27:46 +0100

    Feat: Se agregan 4 cartas especiales con habilidades unicas para Mounstro

--- src/Mounstro.java
+++ src/Mounstro.java
@@ -19,4 +19,34 @@ public class Mounstro extends Entidad {
         return ferocidad / 2;
     }
 
+    public void cartaRugidoAtronador() {
+        System.out.println(getNombre() + " emite un RUGIDO ATRONADOR");
+        System.out.println("Todos los enemigos retroceden asustados");
+    }
+
+    public void cartaGarraDesgarradora() {
+        System.out.println(getNombre() + " usa GARRA DESGARRADORA");
+        System.out.println("Causa sangrado que dura 3 turnos");
+    }
+
+    public void cartaTerremoto() {
+        System.out.println(getNombre() + " provoca un TERREMOTO");
+        System.out.println("Daño a todos los enemigos en el suelo");
+    }
+
+    public void cartaEngullir() {
+        System.out.println(getNombre() + " intenta ENGULLIR al enemigo");
+        System.out.println("Devora enemigos con poca vida");
+    }
+
+    public void enloquecer() {
+        System.out.println(getNombre() + " ENLOQUECE de rabia!");
+        ferocidad += 20;
+        recibirDaño(10);
+        System.out.println("Ferocidad aumentada a " + ferocidad);
+    }
+
+    public int getFerocidad() {
+        return ferocidad;
+    }
 }
